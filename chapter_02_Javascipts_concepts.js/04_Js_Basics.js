411bf43823bb00750cabbbe98b85ea6d0acc0981
Amar Mallick <amarmallick.sqa@gmail.com>
2026-05-09 10:44:09 +0530

    Add chapter_02 and chapter_03 files covering JS concepts, identifiers, literals

---
+++ chapter_02_Javascipts_concepts.js/04_Js_Basics.js
@@ -0,0 +1,14 @@
+// Keywords:Reserved Keyword of any Programing language.
+//Identifier : Variable name
+//Literal : Value of Variable
+//Operator : Symbol which is helps to perform some operations.
+
+var v =15;
+console.log(v);
+v =20;
+console.log(v);
+
+
+
+
+
